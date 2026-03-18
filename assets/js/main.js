1d96249e10563a6c1726e86db9f43534a4a7555c
andrealoda <andrea@casaloda.it>
2026-03-18 22:11:57 +0100

    Milestone 2 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,15 @@ const distanceInput = document.getElementById('distance-input');
 const ageInput = document.getElementById('age-input');
 const formInput = document.querySelector('form');
 
+const nameLi = document.getElementById('name');
+const distanceLi = document.getElementById('distance');
+const ageLi = document.getElementById('age');
+const ticketPriceLi = document.getElementById('ticket-price');
 
 
 
+// leggo i valori inseriti dall'utente e calcolo il prezzo in una funzione che stampa in console.
+// Aggiungo la stampa in pagina.
 formInput.addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -18,14 +24,19 @@ formInput.addEventListener('submit', function (e) {
 
     const costPerKm = 0.21;
     let ticketPrice = distanceInputValue * costPerKm;
-    
+
     if (ageInputValue < 18) {
         ticketPrice = ticketPrice * 0.8;
     } else if (ageInputValue > 65) {
         ticketPrice = ticketPrice * 0.6;
     }
     
-    console.log(nameInputValue, distanceInputValue, ageInputValue, ticketPrice);
+    nameLi.innerHTML = `Nome e Cognome del passeggero: ${nameInputValue}`;
+    distanceLi.innerHTML = `Distanza da percorrere: ${distanceInputValue}`;
+    ageLi.innerHTML = `Età del passeggero: ${ageInputValue}`;
+    ticketPriceLi.innerHTML = `Prezzo del biglietto: ${ticketPrice.toFixed(2)}`;
 
-}
-)
+    console.log(nameInputValue, distanceInputValue, ageInputValue, ticketPrice.toFixed(2));
+
+    formInput.reset();
+})
